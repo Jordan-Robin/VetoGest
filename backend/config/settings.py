4a9207c19dbc681929e2160cd5bccfec9d76bac0
Jordan Robin <jordanrobin100@gmail.com>
2026-01-18 18:24:31 +0100

    feat(core): premiers ajustements pour mise en production

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,6 +31,9 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
+if not DEBUG:
+    ALLOWED_HOSTS = [env('RAILWAY_STATIC_URL'), 'localhost', '127.0.0.1']
+
 CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS', default=['http://localhost:5173'])
 
 CSRF_TRUSTED_ORIGINS = [
@@ -154,3 +157,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
